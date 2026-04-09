e5b1eae885ce5ff4fae03b3c00dc71b522d948e4
soumyaranjanpuhan <soumyaranjan.puhan@33crores.com>
2026-04-09 11:27:29 +0530

    f

--- routes/web.php
+++ routes/web.php
@@ -9,14 +9,6 @@ Route::get('/', function () {
     return view('welcome');
 })->name('home');
 
-/*
-|--------------------------------------------------------------------------
-| Admin Login Routes
-|--------------------------------------------------------------------------
-| gaumitra.com/admin              -> login page
-| gaumitra.com/admin/login        -> login submit
-| gaumitra.com/admin/dashboard    -> admin dashboard
-*/
 Route::prefix('admin')->group(function () {
     Route::get('/', [AdminAuthController::class, 'showLogin'])->name('admin.login');
     Route::post('/login', [AdminAuthController::class, 'login'])->name('admin.login.submit');
@@ -27,13 +19,6 @@ Route::prefix('admin')->group(function () {
     });
 });
 
-/*
-|--------------------------------------------------------------------------
-| Super Admin Routes
-|--------------------------------------------------------------------------
-| Keep this only if you already have normal Laravel auth setup.
-| If not needed now, you can comment this block.
-*/
 Route::middleware(['auth', 'role:superadmin'])->prefix('superadmin')->group(function () {
     Route::get('/dashboard', [SuperAdminDashboardController::class, 'index'])
         ->name('superadmin.dashboard');
